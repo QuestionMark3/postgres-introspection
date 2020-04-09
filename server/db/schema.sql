c7a90351597505494f11cacfd776dfe67cf7a7e6
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2020-04-09 19:26:45 -0400

    Added comments for readability

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,13 +1,16 @@
-SELECT pk.table_name, pk.primary_key, fk.foreign_keys, data.columns
+SELECT pk.table_name, pk.primary_key, fk.foreign_keys, td.columns
 
-FROM (
+FROM (                                            --  Primary key data (pk)
+---------------------------------------------------------------------------
   SELECT  conrelid::regclass AS table_name,
           substring(pg_get_constraintdef(oid), '\((.*?)\)')  AS primary_key
   FROM pg_constraint
   WHERE  contype = 'p' AND connamespace = 'public'::regnamespace
+---------------------------------------------------------------------------
 ) AS pk
 
-INNER JOIN (
+INNER JOIN (                                                   -- Foreign key data (fk)
+---------------------------------------------------------------------------------------
   SELECT conrelid::regclass AS table_name, json_agg(
     json_build_object(
       'name',            substring(pg_get_constraintdef(oid), '\((.*?)\)'),
@@ -18,10 +21,12 @@ INNER JOIN (
   FROM pg_constraint
   WHERE  contype = 'f' AND connamespace = 'public'::regnamespace
   GROUP BY table_name
+---------------------------------------------------------------------------------------
 ) AS fk
 ON pk.table_name = fk.table_name
 
-INNER JOIN (
+INNER JOIN (                                   -- Table data (td)
+-----------------------------------------------------------------
   SELECT tab.table_name, json_agg(
     json_build_object(
       'column_name',              col.column_name,
@@ -31,12 +36,17 @@ INNER JOIN (
       'is_nullable',              col.is_nullable
     )
   ) AS columns
+
+  -- Table names
   FROM (
     SELECT table_name FROM information_schema.tables
     WHERE table_type='BASE TABLE' AND table_schema='public'
   ) AS tab
+
+  -- Table columns
   INNER JOIN information_schema.columns AS col
   ON tab.table_name = col.table_name
   GROUP BY tab.table_name
-) AS data
-ON data.table_name::regclass = pk.table_name
+-----------------------------------------------------------------
+) AS td
+ON td.table_name::regclass = pk.table_name
